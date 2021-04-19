48530b408a35db65249e35cda766300e7cee0261
jinseu <su_stone2012@hotmail.com>
2021-04-20 01:33:02 +0800

    init

---
+++ TODO.md
@@ -0,0 +1,5 @@
+1. 将md文件渲染成html
+2. 将img等资源文件统一放到一个目录
+3. html文件添加导航栏，标签等组件
+
+https://vuematerial.io/
---
+++ src/build.js
@@ -0,0 +1,5 @@
+var showdown  = require('showdown'),
+    converter = new showdown.Converter(),
+    text      = '# hello, markdown!',
+    html      = converter.makeHtml(text);
+console.log(html);
